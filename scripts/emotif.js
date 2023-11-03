96dfa39f98b1997965c38bc880e59ac08bca1d1f
David Gaitsgory <dgaitsgo@student.42.fr>
2023-11-03 16:46:07 +0100

    favicons

---
+++ scripts/emotif.js
@@ -0,0 +1,35 @@
+const r = 200
+const d = r * 2
+
+// const svg = d3.select('#svg-wrapper')
+//     .append('svg')
+//     .attr('viewBox', `0 0 ${d} ${d}`)
+//     .attr('width', r + 1)
+//     .attr('height', r + 1)
+//     .style('position', 'absolute')
+
+// const circ = svg.append('circle')
+//     .attr('cx', r)
+//     .attr('cy', r)
+//     .attr('r', r)
+//     .attr('fill', 'white')
+
+
+// const circ = svg.append('rect')
+//     .attr('x', 0)
+//     .attr('y', 0)
+//     .attr('width', r)
+//     .attr('height', r)
+//     .attr('fill', 'white')
+
+// const logo = document.getElementById('main-logo')
+// const logoRect = logo.getBoundingClientRect()
+
+// console.log(logoRect.right - logoRect.left)
+
+// document.getElementById('main-logo').play()
+
+// svg.transition()
+//     .duration(600)
+//     .style('left', `${logoRect.right - logoRect.left - r}px`)
+    // .attr('cy', ) 
